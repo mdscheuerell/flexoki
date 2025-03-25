8f7085f806649b7b900d915b4e81ef986806c5ed
Mark Scheuerell <scheuerl@uw.edu>
2025-03-25 06:30:58 -0700

    created df for internal pkg use

--- dev/00_scrape-flexoki-readme.R
+++ dev/00_scrape-flexoki-readme.R
@@ -1,11 +1,18 @@
 ## this script scrapes color palette info from
 ## https://github.com/kepano/flexoki/blob/main/README.md
+## and converts it to a usable data frame
 
 #### requirements ####
 
 ## packages
-# readr
-# read.so
+# require(readr)
+# require(read.so)
+# require(tidyr)
+# require(stringr)
+# require(usethis)
+
+
+#### scrape color info ####
 
 ## grab entire README.md
 fn <- "https://raw.githubusercontent.com/kepano/flexoki/refs/heads/main/README.md"
@@ -19,13 +26,37 @@ for(i in seq(from = 0, by = 18, length = 8)) {
   tmp <- rbind(tmp, read.so::read.md(mdc[3:17 + i]))
 }
 
+## nicer col names
+colnames(tmp) <- colnames(tmp) |>
+  tolower()
 
+## split name into color and saturation
+tmp <- tmp |> 
+  tidyr::separate_wider_delim(
+    name,
+    delim = "-",
+    names = c("color", "saturation")
+  )
+## convert sat to integer
+tmp$saturation <- as.integer(tmp$saturation)
 
-# df %>%
-#   tidyr::separate_wider_delim(
-#     field,
-#     delim = " ",
-#     names = c("a", "b"),
-#     too_few = "align_start",
-#     too_many = "merge"
-#   )
+## remove backticks
+tmp$hex <- tmp$hex |>
+  stringr::str_replace_all(pattern = "`", replace = "")
+tmp$rgb <- tmp$rgb |>
+  stringr::str_replace_all(pattern = "`", replace = "")
+
+## split RGB field
+tmp <- tmp |>
+  tidyr::separate_wider_delim(
+    rgb,
+    delim = ", ",
+    names = c("red", "green", "blue")
+  )
+tmp[, c("red", "green", "blue")] <- tmp[, c("red", "green", "blue")] |>
+  lapply(as.integer)
+
+## store color data in package
+flex_colors <- tmp
+usethis::use_data(flex_colors, internal = TRUE)
+
